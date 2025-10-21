bad322b1d44651881e70d929977af6ec3c84100a
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-21 21:57:43 +0300

    feat: done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { access, writeFile } from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+
 const create = async () => {
-  // Write your code here
+  const filePath = path.join(process.cwd(), 'src', 'fs', 'files','fresh.txt');
+  try {
+    await access(filePath, constants.F_OK);
+   throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT'){
+    await writeFile(filePath, "I am fresh and young", { encoding: "utf8" });
+    return;
+    }
+  }
+  throw new Error('FS operation failed');
 };
 
 await create();
