8faf5a7ed2118f85261244a72c49b6bb1ec2c9f1
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-21 22:27:01 +0300

    feat: done copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { access, mkdir, copyFile, cp } from "fs/promises";
+import path from "path";
+import { constants } from "fs";
+
+
 const copy = async () => {
-  // Write your code here
+ const srcDir = path.join(process.cwd(), "src", "fs", "files");
+ const srcDir1 = path.join(process.cwd(), "src", "fs", "files_copy");
+
+ try{
+  await access(srcDir, constants.F_OK);
+  }catch(err){
+ throw new Error("FS operation failed");
+ }
+  try {
+    await access(srcDir1, constants.F_OK);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await mkdir(srcDir1, {recursive: true});
+      await cp(srcDir, srcDir1, { recursive: true })
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await copy();
