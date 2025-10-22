7b2992d52357073f0a331cbb9d8342734a4b1657
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-22 21:33:20 +0300

    feat: done read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { access, readFile } from "fs/promises";
+import path from "path";
+import { constants } from "fs";
+
 const read = async () => {
-  // Write your code here
+    const filePath = path.join(
+      process.cwd(),
+      "src",
+      "fs",
+      "files",
+      "fileToRead.txt"
+    );
+  
+    try{
+      await access(filePath, constants.F_OK);
+      } catch(err){
+      throw new Error("FS operation failed");
+    }
+    const data = await readFile(filePath, { encoding: "utf8" });
+    console.log(data);
 };
 
 await read();
