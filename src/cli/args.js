9abcf500c1ecf75d6e3d0d7140096079732d4d61
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-22 22:09:54 +0300

    feat: done env and args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-  // Write your code here
+   const argv = process.argv.slice(2);
+   const result = [];
+   for (let i = 0; i < argv.length; i += 2) {
+     const keyRaw = argv[i];
+     const val = argv[i + 1];
+     const key = keyRaw.startsWith("--") ? keyRaw.slice(2) : keyRaw;
+     result.push(`${key} is ${val}`);
+   }
+   console.log(result.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-  // Write your code here
+  // let i = 1;
+  let envArr = [];
+  for (const [key, value] of Object.entries(process.env)) {
+    
+    if (key.startsWith('RSS_')) {
+      envArr.push(`${key}=${value}`);
+      // console.log(`${key}=${value}`);
+      // i++
+    }
+  }
+  console.log(envArr.join('; '));
 };
 
 parseEnv();
+
+
