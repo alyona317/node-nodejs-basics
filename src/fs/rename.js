47062d3f9b516134a019bb14de4039d882b64ac3
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-22 21:07:31 +0300

    feat: done rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,39 @@
+import { access} from "fs/promises";
+import { rename as fsRename } from "fs/promises";
+import path from "path";
+import { constants } from "fs";
+
 const rename = async () => {
-  // Write your code here
+  const properFilename = path.join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "properFilename.md"
+  );
+  const wrongFilename = path.join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "wrongFilename.txt"
+  );
+  try {await access(wrongFilename, constants.F_OK);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await access(properFilename, constants.F_OK);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fsRename(wrongFilename, properFilename);
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await rename();
+
