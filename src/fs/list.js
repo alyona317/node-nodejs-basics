1215749c617ab014459a8199c51bb7d9dd909cad
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-22 21:24:35 +0300

    feat: done list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,7 +1,23 @@
+import { access, readdir } from "fs/promises";
+import path from "path";
+import { constants } from "fs";
+
 const list = async () => {
-  // Write your code here
+   const folderPath = path.join(
+     process.cwd(),
+     "src",
+     "fs",
+     "files"
+   );
+ 
+   try{
+     await access(folderPath, constants.F_OK);
+     } catch(err){
+      throw new Error("FS operation failed");
+   }
+    const files = await readdir(folderPath);
+    console.log(files);
 };
 
 await list();
 
-// 
