1983b24a8aa5f2aeced7d358b305c614d162095f
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-23 22:25:10 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import path from "path";
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath =path.join(process.cwd(), "src", "hash", "files", "fileToCalculateHashFor.txt");
+
+  return new Promise((resolve, reject) => {
+  const hash = createHash("sha256");
+  const stream = createReadStream(filePath);
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+  stream.on("end", () => {
+    const sha256Hash = hash.digest("hex");
+    console.log(`SHA-256 хеш: ${sha256Hash}`);
+    resolve(sha256Hash);
+  });
+  stream.on("error", (error) => {
+    reject(error);
+  });
+  })
 };
 
 await calculateHash();
