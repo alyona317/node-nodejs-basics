cdce75aa82985bd879d70734f41b9660ca085c1f
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-26 17:47:26 +0300

    feat: compress an decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import { pipeline } from "stream/promises";
+import { createReadStream } from "fs";
+import { createWriteStream } from "fs";
+import { unlink } from "fs/promises";
+import zlib from "zlib";
+import path from "path";
+
 const compress = async () => {
-  // Write your code here
+  const inputFile = path.join(
+    process.cwd(),
+    "src", "zip", "files", "fileToCompress.txt")
+  const outputFile = path.join(
+    process.cwd(),
+    "src",
+    "zip",
+    "files",
+    "archive.gz"
+  );
+  const gzip = zlib.createGzip();
+
+  try{
+    await pipeline(
+      createReadStream(inputFile),
+      gzip,
+      createWriteStream(outputFile)
+    );
+    await unlink(inputFile);
+    }catch(err){
+      throw err;
+  }
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,37 @@
+import { pipeline } from "stream/promises";
+import { createReadStream } from "fs";
+import { createWriteStream } from "fs";
+import { unlink } from "fs/promises";
+import zlib from "zlib";
+import path from "path";
+
 const decompress = async () => {
-  // Write your code here
+  const inputFile = path.join(
+      process.cwd(),
+      "src",
+      "zip",
+      "files",
+      "archive.gz"
+    );
+  const outputFile = path.join(
+    process.cwd(),
+    "src", 
+    "zip", 
+    "files", 
+    "fileToCompress.txt")
+  const gzip = zlib.createGunzip();
+    try{
+      await pipeline(
+        createReadStream(inputFile),
+        gzip,
+        createWriteStream(outputFile)
+      );
+      await unlink(inputFile);
+      }catch(err){
+        throw err;
+    }
+
+
 };
 
 await decompress();
