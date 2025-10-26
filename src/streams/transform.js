3f8a2d5f41e479e592f1f9c65ccd4d21dc316927
alyona317 <oschepkova.alyona@yandex.ru>
2025-10-26 17:08:02 +0300

    feat: streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+
 const transform = async () => {
-  // Write your code here
+
+  const reverseTransform = new Transform({
+    transform(chunk, _endcoding, callback) {
+      const reverseChunck = chunk.toString().split("").reverse().join("");
+      callback(null, reverseChunck)
+    }
+  })
+  await pipeline(process.stdin, reverseTransform, process.stdout);
 };
 
 await transform();
